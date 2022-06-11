cc57a05d0cd9c20c185380cfb2521cba08046a49
Amanda McCann <amanda@technomancy.org>
2022-06-11 11:38:20 +0200

    misc

--- main.js
+++ main.js
@@ -87,3 +87,11 @@ function fraction(total, passed) {
 	return -Math.log2(1 - (passed/total));
 }
 
+// Register service worker to control making site work offline
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(() => { console.log('Service Worker Registered'); });
+}
+
