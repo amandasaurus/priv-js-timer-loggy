8063dcb96dca63aac2ebf1fb9d18dd0839e4f0dc
Amanda McCann <amanda@technomancy.org>
2022-06-11 11:52:42 +0200

    misc

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
-console.log("ok");
+self.addEventListener('install', (e) => {
+	console.log("install");
+  e.waitUntil(
+    caches.open('counter-store').then((cache) => cache.addAll([
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
