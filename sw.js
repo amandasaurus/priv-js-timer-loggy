1403415a314d7fab8db97d984d7fe6ca5d45c39b
Amanda McCann <amanda@technomancy.org>
2022-06-27 18:22:16 +0200

    misc

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-self.addEventListener('install', (e) => {
-	e.waitUntil(
-		caches.open('counter-store').then((cache) => cache.addAll([
-			"alpinejs-persist-4.10.2-cdn.min.js",
-			"alpinejs-3.10.2-cdn.min.js",
-			"style.css",
-			"main.js",
-			"sw.js",
-			"index.html",
-			"counter.webmanifest",
-			"control_knobs_192.png"
-		]));
-	);
-});
+//self.addEventListener('install', (e) => {
+//	e.waitUntil(
+//		caches.open('counter-store').then((cache) => cache.addAll([
+//			"alpinejs-persist-4.10.2-cdn.min.js",
+//			"alpinejs-3.10.2-cdn.min.js",
+//			"style.css",
+//			"main.js",
+//			"sw.js",
+//			"index.html",
+//			"counter.webmanifest",
+//			"control_knobs_192.png"
+//		]));
+//	);
+//});
 
 //self.addEventListener('fetch', function(event) {
 //  event.respondWith(
